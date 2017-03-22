00de4ad4005177015604688f2489b19361c672fb
olegyurchenko <oleg.yurchenko@gmail.com>
2017-03-22 12:22:16 +0200

    Make pmain activity

--- app/src/main/java/oleg/home/ua/cryptonotes/MainActivity.java
+++ app/src/main/java/oleg/home/ua/cryptonotes/MainActivity.java
@@ -3,13 +3,27 @@ package oleg.home.ua.cryptonotes;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import static android.text.InputType.TYPE_CLASS_TEXT;
+import static android.text.InputType.TYPE_TEXT_VARIATION_PASSWORD;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener,
+  CompoundButton.OnCheckedChangeListener {
+
+  EditText passwordEdit, decryptedEdit, encryptedEdit;
+  CheckBox showPasswordBox;
+  Button encryptButton, decryptButton;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -18,15 +32,21 @@ public class MainActivity extends AppCompatActivity {
     Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
     setSupportActionBar(toolbar);
 
-    FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-    fab.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View view) {
-        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-          .setAction("Action", null).show();
-      }
-    });
-  }
+
+    passwordEdit = (EditText)findViewById(R.id.passwordEdit);
+    decryptedEdit = (EditText)findViewById(R.id.decryptedTextEdit);
+    encryptedEdit = (EditText)findViewById(R.id.encyiptedTextEdit);
+
+    showPasswordBox = (CheckBox)findViewById(R.id.showPassCheck);
+
+    encryptButton = (Button) findViewById(R.id.encryptButton);
+    decryptButton = (Button) findViewById(R.id.decryptButton);
+
+    encryptButton.setOnClickListener(this);
+    decryptButton.setOnClickListener(this);
+    showPasswordBox.setOnCheckedChangeListener(this);
+
+   }
 
   @Override
   public boolean onCreateOptionsMenu(Menu menu) {
@@ -48,5 +68,30 @@ public class MainActivity extends AppCompatActivity {
     }
 
     return super.onOptionsItemSelected(item);
+  }
+
+  @Override
+  public void onClick(View v)
+  {
+    int id = v.getId();
+    switch(id)
+    {
+      case R.id.decryptButton:
+        break;
+      case R.id.encryptButton:
+        break;
+    }
+  }
+
+  @Override
+  public void onCheckedChanged(CompoundButton v, boolean isChecked) {
+    int id = v.getId();
+
+    switch(id)
+    {
+      case R.id.showPassCheck:
+        passwordEdit.setTransformationMethod(isChecked ? null : new PasswordTransformationMethod());
+        break;
+    }
   }
 }
